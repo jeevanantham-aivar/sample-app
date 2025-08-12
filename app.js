55ebfe06a7772cecbb37f1b2125b35fdc9769b67
jeevanantham-aivar <jeevanantham.n@aivar.tech>
2025-08-12 11:24:45 +0530

    changed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 Hello from Sample App running on ECS Fargate! ');
+  res.send('🚀 Hello from Sample App running on ECS Fargate! added codepipeline');
 });
 
 app.listen(PORT, () => {
