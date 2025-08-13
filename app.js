c36e5c4d52abb97eca2d1f226d14139fa81ddc0f
jeevanantham-aivar <jeevanantham.n@aivar.tech>
2025-08-13 10:30:10 +0530

    changed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('🚀 n ECS Fargate! added  added pipeline and chec   king whether a pipeline is working or not ');
+  res.send(' n ECS Faghhghghgghghrgate! added  added pipeline and chec   king whether a pipeline is working or not ');
 });
 
 app.listen(PORT, () => {
